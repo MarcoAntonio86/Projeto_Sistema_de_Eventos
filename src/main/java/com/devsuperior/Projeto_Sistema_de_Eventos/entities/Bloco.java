ab7d96d2118db33c9489d5791bbd26ad3932a46f
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2025-05-03 16:59:32 -0300

    Create classes

---
+++ src/main/java/com/devsuperior/Projeto_Sistema_de_Eventos/entities/Bloco.java
@@ -0,0 +1,44 @@
+package com.devsuperior.Projeto_Sistema_de_Eventos.entities;
+
+import java.time.Instant;
+
+public class Bloco {
+
+	private Integer id;
+	private Instant inicio;
+	private Instant fim;
+	
+	public Bloco(Integer id, Instant inicio, Instant fim) {
+		super();
+		this.id = id;
+		this.inicio = inicio;
+		this.fim = fim;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Instant inicio) {
+		this.inicio = inicio;
+	}
+
+	public Instant getFim() {
+		return fim;
+	}
+
+	public void setFim(Instant fim) {
+		this.fim = fim;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/devsuperior/Projeto_Sistema_de_Eventos/entities/Categoria.java
@@ -0,0 +1,32 @@
+package com.devsuperior.Projeto_Sistema_de_Eventos.entities;
+
+public class Categoria {
+	
+	private Integer id;
+	private String descricao;
+	
+	public Categoria(Integer id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
