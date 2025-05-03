e68a2d26899db0d7e6ed06fbac837c703499fc90
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2025-05-03 16:00:54 -0300

    Initial project commit

---
+++ src/main/java/com/devsuperior/Projeto_Sistema_de_Eventos/ProjetoSistemaDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Projeto_Sistema_de_Eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSistemaDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSistemaDeEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/Projeto_Sistema_de_Eventos/ProjetoSistemaDeEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Projeto_Sistema_de_Eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSistemaDeEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
